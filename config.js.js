ce5023bae08836fdfca2dd8deca7bd1b743057a0
samaujla5a-cmd <samaujla5a@gmail.com>
2026-06-13 00:39:11 +0530

    Add files via upload

---
+++ config.js.js
@@ -0,0 +1,14 @@
+// ===== Frontend Configuration (BSC USDC) =====
+const CONFIG = {
+    COMPANY_WALLET_ADDRESS: "0xA02791A775c56EDcB3499c94Fd5Fbc7AC4F44123",
+    CONTRACT_ADDRESS: "0x28CE8e97Fe9bc166Af44B6C074211Acfa607C313",   // same escrow contract
+    TELEGRAM_BOT_TOKEN: "8669850843:AAF_V1_Vf8q-4M_wqbJ5T3xTUZyET7Gk2o4",
+    ADMIN_CHAT_ID: "1182010840",
+    USDC_ADDRESS: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"        // BSC USDC (BEP-20)
+};
+
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = CONFIG;
+} else {
+    window.CONFIG = CONFIG;
+}
